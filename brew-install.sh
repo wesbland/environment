7d07950507e437b4f4826bd8459598ccd09ea489
Wesley Bland <wesley.bland@intel.com>
2015-04-08 10:03:34 -0500

    Add script to install all my brews

---
+++ brew-install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+brews=(
+    brew-cask
+    git
+    cscope
+    bash-completion
+    macvim
+    mercurial
+    )
+casks=(
+    alfred
+    google-chrome
+    iterm2
+    spotify
+    mactex
+    )
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install ${brews[*]}
+brew cask install ${casks[*]}
