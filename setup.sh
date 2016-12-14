1454df8a48a984a6d56251710eb6fbf38d84c1c4
Wesley Bland <wesley.bland@intel.com>
2016-12-14 17:10:00 -0600

    Remove .nvimrc

--- setup.sh
+++ setup.sh
@@ -5,7 +5,6 @@ ln -s -f $PWD/gitconfig $HOME/.gitconfig
 ln -s -f $PWD/hgrc $HOME/.hgrc
 ln -s -f $PWD/vim/ $HOME/.vim
 ln -s -f $PWD/vim/vimrc $HOME/.vimrc
-ln -s -f $PWD/vim/vimrc $HOME/.nvimrc
 ln -s -f $PWD/screenrc $HOME/.screenrc
 ln -s -f $PWD/tmux.conf $HOME/.tmux.conf
 ln -s -f $PWD/bash_completion $HOME/.bash_completion
